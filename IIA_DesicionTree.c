4ee75f10848dda388397ceb5cfa2b1ec7fec0e0f
luroel <jl.rodrigueze@hotmail.com>
2024-05-30 13:56:22 -0600

    Archivos excel con las calidades del vino 20/1580 muestras + abstracción de los datos excel en una variable wr

---
+++ IIA_DesicionTree.c
@@ -0,0 +1,8 @@
+%--------------------------Desicion Tree Learning----------------------------%
+%---------------Introducción a la Inteligencia Artificial--------------------%
+%------------------------LUIS RODRIGUEZ ELIAS-----------------------------%
+
+clear all
+clc
+
+wq = readtable('winequality20n.csv');
